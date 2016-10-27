2f5e155a0631dbd8c51e8a340c01ba5a0aba5745
Elijah <elieel@gmail.com>
2016-10-27 12:48:17 -0400

    Initial commit. Complete Positive Numbers Ex and Even Numbers Ex

---
+++ functions_practice.js
@@ -0,0 +1,15 @@
+// Positive Numbers:
+// var arr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+// function pos_num(n) {
+//     return n > 0;
+// }
+// var pos_arr = arr.filter(pos_num);
+// console.log(pos_arr);
+
+// Even Numbers
+var arr = [1,2,4,5,6,7,8,9,10];
+function even_num(n) {
+  return n % 2 === 0;
+}
+var ans = arr.filter(even_num);
+console.log(ans);
